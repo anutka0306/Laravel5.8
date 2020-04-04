3100531d37b3eb4c0f7a5828bce388d00c4a1f67
anutka0306 <anutka0306@mail.ru>
2020-04-04 11:33:11 +0300

    add to json

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -17,10 +17,12 @@ class IndexController extends Controller
         if($request->isMethod('post')){
             $newsData = \Illuminate\Support\Facades\File::get(base_path() .'/storage/data/news.json');
             $newsData = json_decode($newsData, true);
+            $next_id = end($newsData);
+            $next_id = $next_id['id']+1;
            $inputData = $request->except(['_token']);
-            $inputData['id'] = 6;
+            $inputData['id'] = $next_id;
             array_push($newsData, $inputData);
-            \Illuminate\Support\Facades\File::put(base_path() .'/storage/data/news.json', json_encode($newsData));
+            \Illuminate\Support\Facades\File::put(base_path() .'/storage/data/news.json', json_encode($newsData, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
             return $inputData;
            //dd($request->except(['_token']));
             //return redirect()->route('admin.create');
