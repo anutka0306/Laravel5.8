521d9d62c5c32c1a63675fa24c83f1ba495fb8a2
anutka0306 <anutka0306@mail.ru>
2020-04-18 09:49:42 +0300

    change profile data

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function update(Request $request){
         $user = Auth::user();
+        $errors = [];
+        if($request->isMethod('post')){
+            if(Hash::check($request->post('password'), $user->password)){
+                $user->fill(
+                    [
+                        'name'=>$request->post('name'),
+                        'password'=>Hash::make($request->post('new-password')),
+                        'email'=>$request->post('email')
+                    ]
+                );
+                $user->save();
+                $request->session()->flash('success','Данные пользователя изменены успешно');
+            }else{
+               $errors['password'][] = 'Неверно введен текущий пароль';
+            }
+            return redirect()->route('admin.updateProfile')->withErrors($errors);
+        }
         return view('admin.profile',[
            'user'=>$user
         ]);
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.admin')
 
 @section('content')
     <div class="container">
@@ -54,10 +54,10 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Новый пароль</label>
+                                <label for="new-password" class="col-md-4 col-form-label text-md-right">Новый пароль</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <input id="new-password" type="password" class="form-control" name="new-password" required autocomplete="new-password">
                                 </div>
                             </div>
 
