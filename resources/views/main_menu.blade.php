1169348936dbea14501ec87bc199eeb4c8c296fb
anutka0306 <anutka0306@mail.ru>
2020-04-25 17:10:21 +0300

    escape admin menu for no admins

--- resources/views/main_menu.blade.php
+++ resources/views/main_menu.blade.php
@@ -3,4 +3,8 @@
     <li class="nav-item {{ request()->routeIs('About')?'active':'' }}"><a class="nav-link" href="{{ route('About') }}">О сервисе</a></li>
     <li class="nav-item {{ request()->routeIs('news.category.index')?'active':'' }}"><a class="nav-link" href="{{ route('news.category.index') }}">Категории</a></li>
     <li class="nav-item {{ request()->routeIs('news.index')?'active':'' }}"><a class="nav-link" href="{{ route('news.index') }}">Новости</a></li>
-    <li class="nav-item"> <a class="nav-link" href="{{ route('admin.news.index') }}">Adminka</a></li>
+    @auth
+    @if(\Illuminate\Support\Facades\Auth::user()->isAdmin)
+        <li class="nav-item"> <a class="nav-link" href="{{ route('admin.news.index') }}">Adminka</a></li>
+        @endif
+    @endauth
