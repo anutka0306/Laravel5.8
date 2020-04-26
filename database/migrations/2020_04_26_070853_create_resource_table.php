4f1b03e006b47eee797831d16df9b4c7fa151d56
anutka0306 <anutka0306@mail.ru>
2020-04-26 10:12:55 +0300

    resource migration

---
+++ database/migrations/2020_04_26_070853_create_resource_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResourceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resource', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('link');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resource');
+    }
+}
